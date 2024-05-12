c1df39305438e9e4762f37b39466cc83e04995d1
Chris <nzeanoruec@gmail.com>
2024-05-12 13:47:01 -0400

    Added styled-component

--- src/containers/alert/Alert.tsx
+++ src/containers/alert/Alert.tsx
@@ -1,22 +1,23 @@
 import { BtnPrimary } from "../../components";
 import { useCompose } from "../../hooks";
+import * as AlertStyle from "./AlertStyle";
 
 const Alert = () => {
 	const { useComposeMessage } = useCompose();
 	return (
 		<>
-			<div className="" style={{ position: "absolute", top: "0", left: "0", right: "0", width: "100%", height: "100%", zIndex: "100000000", cursor: "pointer" }}>
-				<div style={{ position: "absolute", top: "15%", left: "50%", right: "50%", transform: "translate(-50%, -50%)", boxShadow: "0 0 2px rgba(0,0,0,.5)", borderRadius: "5px", padding: "2rem", width: "400px", height: "150px", backgroundColor: "#fff", cursor: "default" }}>
-					<div className="d-flex flex-column justify-content-between h-100">
+			<AlertStyle.AlertWrapper>
+				<AlertStyle.AlertContainer>
+					<AlertStyle.AlertFlexContainer>
 						<div>The URL is not valid and cannot be loaded.</div>
-						<div className="d-flex justify-content-end mt-3">
+						<AlertStyle.AlertBtnWrapper>
 							<div onClick={useComposeMessage.setAlertOff}>
 								<BtnPrimary name="Cancel" borderRadius="30px" />
 							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+						</AlertStyle.AlertBtnWrapper>
+					</AlertStyle.AlertFlexContainer>
+				</AlertStyle.AlertContainer>
+			</AlertStyle.AlertWrapper>
 		</>
 	);
 };
---
+++ src/containers/alert/AlertStyle.ts
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+const AlertWrapper = styled.div`
+	position: absolute;
+	top: 0;
+	left: 0;
+	right: 0;
+	width: 100%;
+	height: 100%;
+	z-index: 100000000;
+	cursor: pointer;
+`;
+
+const AlertContainer = styled.div`
+	box-shadow: 0 0 2px rgba(0, 0, 0, 0.5);
+	border-radius: 5px;
+	padding: 2rem;
+	margin: 30px auto 0 auto;
+	width: 400px;
+	height: 150px;
+	background-color: #fff;
+	cursor: default;
+`;
+
+const AlertFlexContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	height: 100%;
+`;
+
+const AlertBtnWrapper = styled.div`
+	display: flex;
+	justify-content: end;
+`;
+
+export { AlertWrapper, AlertContainer, AlertFlexContainer, AlertBtnWrapper };
