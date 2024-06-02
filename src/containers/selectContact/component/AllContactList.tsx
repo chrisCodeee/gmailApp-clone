8c65b1f32e82e4e2c56592f8916d7e6a082eae39
Chris <nzeanoruec@gmail.com>
2024-06-02 07:01:15 -0400

    Removed unused variables

--- src/containers/selectContact/component/AllContactList.tsx
+++ src/containers/selectContact/component/AllContactList.tsx
@@ -1,6 +1,6 @@
 import { IoPersonCircleSharp } from "react-icons/io5";
 import { AllContactListWrapper } from "../SelectContactStyle";
-import { MdCheckBox, MdCheckBoxOutlineBlank } from "react-icons/md";
+import { MdCheckBoxOutlineBlank } from "react-icons/md";
 import { useCompose } from "../../../hooks";
 
 export const contactListItems = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40];
@@ -10,7 +10,7 @@ const AllContactList = () => {
 	return (
 		<>
 			<AllContactListWrapper>
-				{contactListItems.map((item, index) => (
+				{contactListItems.map((item) => (
 					<div className="container" key={item} onMouseEnter={() => useComposeMessage.setContactHoverOn(item)}>
 						<div className="user col-7">
 							{useComposeMessage.contactHoverState !== item && (
